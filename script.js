e82c5f7b610d0cc10b7cd8556aeeda3ca4bdabc5
Saundie Weiss <saundieweiss@gmail.com>
2016-01-20 07:28:24 -0700

    Add controller but need ot set up correctly still and write functions for the operations

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-'use strict';
+var app = angular.module("calcApp",[]);
 
-angular.module('calc', [])
-.controller('display', function($scope){
-// $scope.total ={
-//
-// }
+app.controller('display', function($scope) {
+    // var vm = this;
+    // vm.total = 'The total';
 
-});
+    $scope.total= 'scope total!';
+    // $scope.num = ''
+    $scope.add ='';
+    $scope.subtract= '';
+    $scope.multiply= '';
+
+  });
